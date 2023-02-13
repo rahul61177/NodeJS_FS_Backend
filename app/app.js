522a068b7cbcead0001f3d26ed076f4e1ea316b4
Rahul Upadhyaya <rahul61177@gmail.com>
2023-02-13 13:24:19 +0530

    Mongoose Models and Db

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const userRouter = require('../router/userRouter');
+const {connect} = require('../db/db');
 
 const app = express();
 
@@ -36,5 +37,7 @@ app.use((error, req, res, next) => {
         },
     });
 });
+
+connect();
 
 module.exports = app;
